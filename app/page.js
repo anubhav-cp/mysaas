bd025e836580d542aca79cba83a387e5bcfc080a
Anubhav Tiwari <anubhavtiwaricp@gmail.com>
2025-04-18 17:16:28 +0530

    added Image

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import ButtonLogin from "@/components/buttonLogin";
 import FaqListItem from "@/components/faqListItem";
+import Image from "next/image";
+import productDemo from "./productDemo.jpeg"
 
 export default function Home() {
   const isLoggedIn = true;
@@ -22,15 +24,20 @@ export default function Home() {
       </section>
 
       {/* Hero */}
-      <section className="text-center py-32 px-8 max-w-3xl mx-auto">
+      <section className="text-center py-32 lg:text-left px-8 max-w-5xl mx-auto flex flex-col lg:flex-row gap-14 items-center lg:items-start">
+      <Image src={productDemo} alt="Product Demo" className="w-96 rounded-xl"/>
+        <div>
         <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
           Collect customer feedback to build better products
         </h1>
+        
         <div className="opacity-90 mb-10">
           Create a feedback board in minutes, prioritize features, and build
           products your customers will love.
         </div>
         <ButtonLogin hasLoggedIn={isLoggedIn} name={name} />
+        
+        </div>
       </section>
 
       {/* Pricing */}
