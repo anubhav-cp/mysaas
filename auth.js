668e8c89f573d6e121223646f6f57205c9d7431d
Anubhav Tiwari <anubhavtiwaricp@gmail.com>
2025-04-19 15:45:52 +0530

    fixed autoprefixer and make api endpoint, added authjs

---
+++ auth.js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "./libs/mongo";
+
+
+const config = {
+    providers: [],
+    adapter: MongoDBAdapter(clientPromise),
+    
+    
+}
+
+export const {handlers, signIn, signOut, auth} = NextAuth(config)
