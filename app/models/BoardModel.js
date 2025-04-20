6ec7b60dbe7acd6976831a3972f13148f75a048e
Anubhav Tiwari <anubhavtiwaricp@gmail.com>
2025-04-20 23:31:26 +0530

    Added Mongoose Created UserSchema, BoardSchema

---
+++ app/models/BoardModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const BoardSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    }
+})
+
+export default mongoose.models.Board || mongoose.model("Board", BoardSchema)
